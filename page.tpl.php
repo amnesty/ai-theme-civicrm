37629a959a23a16859afbfb6589bf744a1b9bcee
root <root@warlop.AMNISTIA>
2016-01-15 12:20:28 +0100

    remote

--- page.tpl.php
+++ page.tpl.php
@@ -78,7 +78,47 @@ echo '
 
 }
 else {
-    echo '
+?>
+    
+<!-- Header -->
+     <div id="header"><div class="section clearfix">
+
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </strong></div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
+      <?php endif; ?>
+
+      <?php print render($page['header']); ?>
+
+    </div></div> <!-- /.section, /#header -->
+
+
+<!-- Page -->
+
+    <div id="page-wrapper"><div id="page">';
+<?php    print $messages; ?>
+    
     <!-- Bootstrap Grid -->
     <div class="container--wide">
         <div class="grid">
@@ -86,10 +126,8 @@ else {
                 <div class="content-form clearfix">
                 <p class="text-intro">En Amnistía Internacional <span>renunciamos a los fondos de gobiernos y partidos políticos</span> por una simple razón: nuestra independencia está por encima de todo. <span>Nos financiamos gracias a las aportaciones de personas como tú, que deciden apoyarnos económicamente porque quieren un mundo más justo.</span></p>
                     <div class="box-form-es">
-                        <p>Los campos marcados con * son obligatorios.</p>  
-    ';
-    print render($page['content']); 
-    echo '
+                        <p>Los campos marcados con * son obligatorios.</p>
+    <?php print render($page['content']); ?>
                     </div>
                     <div class="box-es-right">
 
@@ -115,6 +153,5 @@ else {
                 </div>
             </div>    
         </div>
-    </div>';
-}
-?>
+    </div>
+</div></div>
