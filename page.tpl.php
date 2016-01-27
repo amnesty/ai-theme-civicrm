1e14c90697cfcebb7f15a4696e4916bcdd4c6d14
Helena <hgarcia@es.amnesty.org>
2016-01-27 18:09:32 +0100

    retoques

--- page.tpl.php
+++ page.tpl.php
@@ -1,3 +1,11 @@
+<script language="javascript" type="text/javascript">
+    function popitup(url) {
+        newwindow=window.open(url,'name','height=200,width=150');
+        if (window.focus) {newwindow.focus()}
+        return false;
+    }
+</script>
+
 <div id="page-wrapper" onload="errors()"><div id="page"><div id="content" class="clearfix">
 
 <?php
