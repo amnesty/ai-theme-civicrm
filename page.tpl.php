de612fe4906f0c245a703b5a17f49f87b71909ac
Helena <hgarcia@es.amnesty.org>
2016-01-27 16:45:28 +0100

    añadidas librerías Bootstrap

--- page.tpl.php
+++ page.tpl.php
@@ -8,10 +8,7 @@ include_once('config.php');
 if ($node->nid==$form_node){ // Sólo para el formulario de Socixs
 
 ?>
-<!-- Bootstrap Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<!-- Bootstrap Latest compiled JavaScript -->
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 <!-- Cargamos los CSS que necesitamos para el contenido genérico -->
 <link rel="stylesheet" type="text/css" href="<?php print $form_path; ?>/css/style-form.css">
 <link rel="stylesheet" type="text/css" href="<?php print $theme_path; ?>/css/ai.css">
