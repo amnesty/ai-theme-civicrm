9678e27725d6ab56fb04718a02a2c2c9be3a24ec
Helena <hgarcia@es.amnesty.org>
2016-04-20 17:28:41 +0200

    pagina de confirmacion

--- html.tpl.php
+++ html.tpl.php
@@ -9,39 +9,13 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+  
    <!-- You can use Open Graph tags to customize link previews.
     Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
   <meta property="og:url"           content="https://crm.es.amnesty.com/unete-a-amnistia" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="Únete a Amnistía Internacional" />
   <meta property="og:description"   content="El mundo puede cambiar, pero no va a cambiar solo" />
-
-  <!--script language="javascript" type="text/javascript">
-    function popitup(url) {
-        newwindow=window.open(url,'name','height=200,width=150');
-        if (window.focus) {newwindow.focus()}
-        return false;
-    }
-  </script-->
-
-  <!--script>
-    var urlActual = 'http://www.es.amnesty.org/gracias-2015';
-    var tituloActual = 'Mira todo lo que se consiguió en 2015'; 
-    var tituloActualTW = 'Mira todo lo que se consiguió en 2015';
-    var urlActualFB = 'http://www.es.amnesty.org/gracias-2015?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=gracias-2015';
-    var urlActualTW = 'https://www.es.amnesty.org/gracias-2015?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=gracias-2015';
-    var urlActualFBFirma = 'http://www.es.amnesty.org/gracias-2015?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=gracias-2015';
-    var urlActualTWFirma = 'https://www.es.amnesty.org/gracias-2015?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=gracias-2015';
-  </script-->
-
-  <?php
-    // Facebook things
-    $title=urlencode('Comparte los derechos humanos.');
-    $url=urlencode('https://crm.es.amnesty.com/unete-a-amnistia');
-    $summary=urlencode('El mundo puede cambiar, pero no va a cambiar solo.');
-    //$image=urlencode('http://www.yourdomain.com/images/share-thumbnail.jpg');
-  ?>
-
 
   <!-- Faacebook -->
   <div id="fb-root"></div>
--- page.tpl.php
+++ page.tpl.php
@@ -94,7 +94,7 @@ if ( $node->nid==$socixs_form || $node->nid==$socixs_gracias) {  ?>
                     <?php // ****************  Sólo mostramos los botones de compartir en la página de gracias ******************
                         if ($node->nid==$socixs_gracias ){ ?>
                             <!-- FB -->
-                            <div class="fb-share-button" data-href="https://pruebacrm.es.amnesty.org/unete-a-amnistia/?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=form_socios" data-layout="button" style="float:left;"></div> <!--data-text="Yo también defiendo los derechos humanos con Amnistía Internacional"-->
+                            <div class="fb-share-button" data-href="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=form_socios" data-layout="button" style="float:left;"></div> <!--data-text="Yo también defiendo los derechos humanos con Amnistía Internacional"-->
 
                             <!-- Twitter -->
                             <div><a href="https://twitter.com/share" class="twitter-share-button" data-url="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=form_socios" 
