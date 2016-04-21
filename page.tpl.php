b468b06cbb5cb8439c0c4bc5ac38c70d5a530415
Helena <hgarcia@es.amnesty.org>
2016-04-21 10:19:00 +0200

    botones de compartir

--- page.tpl.php
+++ page.tpl.php
@@ -94,14 +94,16 @@ if ( $node->nid==$socixs_form || $node->nid==$socixs_gracias) {  ?>
 
                     <?php // ****************  Sólo mostramos los botones de compartir en la página de gracias ******************
                         if ($node->nid==$socixs_gracias ){ ?>
+                        <div id="share-buttons">
                             <!-- FB -->
                             <div class="fb-share-button" data-href="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=form_socios" data-layout="button" style="float:left;"></div>
-                    
+
                             <!-- Twitter -->
                             <div><a href="https://twitter.com/share" class="twitter-share-button" data-url="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=form_socios" 
                             data-text="Yo también defiendo los derechos humanos con Amnistía Internacional" data-via="amnistiaespana">Tweet</a></div>
                             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
-                    <?php } ?>
+                        <?php } ?>
+                        </div>
                 <?php if($node->nid==$socixs_form) { 
                     print '</div><!-- Box FORM_ES -->'; ?>
                 <div class="box-es-right">
