6a3b6be1d2101045603c31dd22bd3a8e316a475f
Helena <hgarcia@es.amnesty.org>
2016-04-25 18:02:17 +0200

    error boxes

--- ai-formulario/js/socixs-form.js
+++ ai-formulario/js/socixs-form.js
@@ -128,10 +128,13 @@ jQuery(function($) {
   // Mark errors in select boxes
 
   $(".error").not(".messages").each( function(){
-    /*if ($(this).is("select")){*/
+    if ($(this).is("select")){
         $(this).parent().addClass("form-error");
         $(this).parent().css("border", "#f00 2px solid");
-    /*}*/
+    }
+    else {
+        $(this).css("border", "#f00 2px solid");
+    }
   });
 
     // Show or hide/erase "other quantity" field depending on which checkbox is checked
