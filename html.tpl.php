0b16befed2e20e2c6f9c42fffb3198bf5a5105a8
Helena <hgarcia@es.amnesty.org>
2016-05-09 11:22:01 +0200

    boton facebook

--- html.tpl.php
+++ html.tpl.php
@@ -11,17 +11,17 @@
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
    <!-- You can use Open Graph tags to customize link previews.
     Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
-  <meta property="og:url"           content="https://crm.es.amnesty.com/unete-a-amnistia/" />
+  <meta property="og:url"           content="https://crm.es.amnesty.org/unete-a-amnistia/" />
   <meta property="og:type"          content="website" />
   <meta name="title" property="og:title"         content="Hazte socio/a de Amnistía Internacional y apoya nuestro trabajo" />
   <meta name="description" property="og:description"   content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos." />
 
   <script>
-    var urlActual = 'https://crm.es.amnesty.com/unete-a-amnistia/';
+    var urlActual = 'https://crm.es.amnesty.org/unete-a-amnistia/';
     var tituloActual = 'Gracias'; 
     var tituloActualTW = 'Gracias';
-    var urlActualFB = 'https://crm.es.amnesty.com/unete-a-amnistia/?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=socios';
-    var urlActualTW = 'https://crm.es.amnesty.com/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=socios';
+    var urlActualFB = 'https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=facebook&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=socios';
+    var urlActualTW = 'https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Amnesty&utm_content=socios';
   </script>
 
 </head>
