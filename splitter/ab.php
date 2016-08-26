1d9e137864ca5c21131c7716611b8f50f250e528
Helena <hgarcia@es.amnesty.org>
2016-08-26 14:39:51 +0200

    added splitter mini-module for A/B testing

---
+++ splitter/ab.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once("testAB.php");
+
+echo "Node: " . $node->nid;
+$testAB = new testAB($node->nid);
+
+// If this node has an enabled test A/B
+if( $testAB->get_id() ){
+
+  // Get the user's IP
+  $remote_ip = $_SERVER['REMOTE_ADDR'];
+  if(!$remote_ip || $remote_ip == ''){
+      $remote_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+
+  // Get the cookie
+  $cookie_info = explode('-', $_COOKIE['aiesp_multivariate_test']);
+  if( isset($cookie_info) && $cookie_info[0] == $testAB->get_id() ){
+      $test_id = $cookie_info[0];
+      $option_id = $cookie_info[1];
+  }
+  else {
+      // If we don't have the cookie, search in the historic (if not, get and save a random option)
+      $option_id = $testAB->search_option($remote_ip);
+
+      // Set the cookie with the option served and the visitor id
+      $cookie_expire = time()+60*60*24*60; // expiration in 60 days
+      $cook = setcookie('aiesp_multivariate_test', $testAB->get_id() . '-' . $option_id, $cookie_expire);
+  }
+
+  // ------------ Test A/B ------------------------
+  // Redirect to version A or B
+  if ($option_id==1){
+    echo "Option A";
+    //return true; // Opción A
+  }
+  else{
+    echo "Option B";
+    $nodeB = $testAB->get_option_node(1);
+    header('Location: /civicrm/node/' . $nodeB . "?op=B");
+  }
+
+}
+
+?>
---
+++ splitter/testAB.php
@@ -0,0 +1,126 @@
+<?php
+
+class testAB {
+
+  private $node;
+  private $test_id; //If it's 0 it means this node doesn't have a current test
+  private $num_variants; //number of variants ($variants.size)
+  private $variants; // array with every variant which contains: title (string), mutations(array):{ target_nid(int), nid(int), plugin(string)}
+  private $conn; //DB connection
+
+  function __construct($node) {
+    $this->node = $node;
+    $conn = Database::getConnection();
+    $this->search_experiment();
+  }
+
+  // ---------- GETTERS -------------
+  public function get_id(){
+    return $this->test_id;
+  }
+
+  public function get_node(){
+    return $this->node;
+  }
+
+  public function get_num_variants(){
+    return $this->$num_variants;
+  }
+
+  public function get_option_node($id){
+    return $this->variants[$id];
+  }
+
+  // ------------- OTHER ---------------
+
+  // searches if the given node is in a enabled A/B test
+  private function search_experiment(){
+    $this->test_id=0;
+    $this->variants = array();
+    $this->num_variants = 0;
+
+    // DB connection
+    /*$result = db_select('multivariate_test', 'mt')
+      ->condition('enabled', 1, '=')
+      ->fields('mt', array('mtid','variants_set'))
+      ->execute();
+
+    while ($record = $result->fetchAssoc()){
+      $test_id = $record['mtid'];
+      $variants = unserialize($record['variants_set'])[0]["variants"];
+      // si el test es sobre el nodo actual
+      if(!is_null($variants) && $variants[0]['mutations'][0]["target_nid"] == $this->node ){
+            $this->test_id = $test_id;
+            $this->variants = $variants;
+            $this->num_variants = count($variants);
+            break;
+      }
+    }*/
+
+    // DB connection
+    $result = db_select('aiesp_multivariate_tests', 'mt') //!!!!
+      ->condition('enabled', 1, '=')
+      ->fields('mt', array('id','node_origin', 'node_a', 'node_b'))
+      ->execute();
+
+    while ($record = $result->fetchAssoc()){
+      $test_id = $record['id'];
+      $node_id = $record['node_origin'];
+      $node_a = $record['node_a'];
+      $node_b = $record['node_b'];
+      // si el test es sobre el nodo actual
+      if( $node_id == $this->node ){
+            $this->test_id = $test_id;
+            array_push($this->variants, $node_a, $node_b);
+            $this->num_variants = count($this->$variants);
+            break;
+      }
+    }
+  }
+
+  // Searches an option in the DB having an ip address and test id
+  public function search_option($remote_ip){
+    $result = db_select('aiesp_multivariate_users', 'u')
+      ->condition('ip_address', $remote_ip, '=')
+      ->condition('test_id', $this->test_id, '=')
+      ->fields('u', array('ip_address','option_id'))
+      ->range(0, 1)
+      ->execute();
+    $record = $result->fetchAssoc();
+    $current_ip = $record['ip_address'];
+    $option_id = $record['option_id'];
+
+    // If is not in the historic, insert it
+    if(!$current_ip){
+      $option_id = $this::get_random();
+      // Insert in the DB
+      $nid = db_insert('aiesp_multivariate_users')
+      ->fields(array(
+        'ip_address' => $remote_ip,
+        'test_id' => $this->test_id,
+        'option_id' => $option_id,
+      ))
+      ->execute();
+    }
+    return $option_id;
+  }
+
+  // ------- STATIC FUNCTIONS ----------
+
+  // Seeds with microseconds
+  private static function make_seed(){
+    list($usec, $sec) = explode(' ', microtime());
+    return (float) $sec + ((float) $usec * 100000);
+  }
+
+  // Get a random number
+  public static function get_random() {
+    mt_srand(testAB::make_seed());
+    $rand = mt_rand(0,1);
+    //$rand = srand((double)microtime()*1000000);
+    return $rand;
+  }
+
+}
+
+?>
