cfab1fcf768c0bd14c8d53396a78860ca1e60b2a
Helena <hgarcia@es.amnesty.org>
2016-08-30 12:08:45 +0200

    [AB splitter]: testing

--- splitter/testAB.php
+++ splitter/testAB.php
@@ -71,6 +71,7 @@ class testAB {
           $node_id = $record['node_origin'];
           $node_a = $record['node_a'];
           $node_b = $record['node_b'];
+          var_dump($node_id);
           // si el test es sobre el nodo actual
           /*if( $node_id == $this->node ){
                 $this->test_id = $test_id;
