f4d367aa65437314015b2f6e7824923d9c55b550
Helena <hgarcia@es.amnesty.org>
2016-08-31 16:33:02 +0200

    [AB splitter]: eliminar cookie antigua

--- splitter/ab.php
+++ splitter/ab.php
@@ -24,12 +24,11 @@ if( $testAB->get_id() ){
       $option_id = $cookie_info[1];
   }
   else {
-      // If we don't have the cookie, search in the historic (if not, get and save a random option)
+      // If we don't have the cookie, search in the historic (if not found, get and save a random option)
       $option_id = $testAB->search_option($remote_ip);
 
       // Unset the previous cookie (from another test, if there is)
       unset($_COOKIE['aiesp_multivariate_test']);
-      var_dump($testAB->get_id());
 
       // Set the cookie with the option served and the visitor id
       $cookie_expire = time()+60*60*24*60; // expiration in 60 days
