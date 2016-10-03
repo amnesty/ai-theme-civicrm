9bce170fb6f091175fb46fe8a7707db6711341f0
frodriguez <frodriguez@es.amnesty.org>
2016-10-03 15:28:04 +0200

    Se añade # para el título

--- page.tpl.php
+++ page.tpl.php
@@ -90,7 +90,7 @@ if (!isset($_GET["origen"]) ){
                               <span class="heading--tape--dark">¡Te damos la bienvenida!</span></span>
                   <?php } ?>
                 <?php } else {?>
-                              <span class="heading--tape--dark"><?php echo explode('#',$node->title)[0] ?></span>
+                              <span class="heading--tape--dark"><?php $title = explode('#',$node->title); echo $title[0]; ?></span>
                 <?php } ?>
             </h2>
         </div>
