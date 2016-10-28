ff96c371eb0615cabca8c4864af9600347bbeb95
Helena <hgarcia@es.amnesty.org>
2016-10-28 16:45:46 +0200

    esconder caja otra cuota

--- ai-formulario/js/donativos.js
+++ ai-formulario/js/donativos.js
@@ -20,20 +20,20 @@ jQuery(function($) {
     }
 
     // Esconder otra cuota
-    if ( $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-1").is(":checked") ||
-        $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-2").is(":checked") ||
-        $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-3").is(":checked")
+    if ( $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-1").is(":checked") ||
+        $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-2").is(":checked") ||
+        $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-3").is(":checked")
     ){
         $(".otra_cuota").css("visibility", "hidden");
     }
     $(".cuota").click( function() {
-      if ( $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-1").is(":checked") ||
-          $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-2").is(":checked") ||
-          $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-3").is(":checked")
+      if ( $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-1").is(":checked") ||
+          $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-2").is(":checked") ||
+          $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-3").is(":checked")
       ){
           $(".otra_cuota").css("visibility", "hidden");
       }
-      if ( $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-4").is(":checked") ){
+      if ( $("#form-item form-type-radio form-item-submitted-caja-colaborar-fieldset-fila-1-0-donativo-4").is(":checked") ){
           $(".otra_cuota").css("visibility", "visible");
       }
     });
