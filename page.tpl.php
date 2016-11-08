fd1759f46fb062480db5827c878f34d047be3a71
Helena <hgarcia@es.amnesty.org>
2016-11-08 17:05:58 +0100

    [formulario donativo]: boton hazte socio/a pagina gracias

--- page.tpl.php
+++ page.tpl.php
@@ -140,8 +140,8 @@ $mobile = (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|
 
                     <?php // ****************  Sólo mostramos los botones de compartir en la página de gracias ******************
                         if($node->nid == $donativos_gracias){ ?>
-                          <div style="width:50%;">
-                            <a class="ai-cta-2col__banner-btn" href="https://crm.es.amnesty.org/unete-a-amnistia/?origen=home">Hazte Socio/a</a>
+                          <div style="width:45%; margin-left: 50px;">
+                            <a class="ai-cta-2col__banner-btn" href="https://crm.es.amnesty.org/unete-a-amnistia/?origen=home">Únete a Amnistía Internacional</a>
                           <div>
                         <?php } else if (in_array($node->nid, $socixs_gracias_list)){ ?>
                         <div id="share-buttons" class="ai-accion-firma-compartir">
