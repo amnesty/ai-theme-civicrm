3aeb34d23c6b64d432313c98d3e906094801c154
Helena <hgarcia@es.amnesty.org>
2016-11-28 11:13:39 +0100

    arregladas twitter cards

--- meta-tags.php
+++ meta-tags.php
@@ -6,6 +6,7 @@
     <meta name="description" property="og:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos." />
     <meta property="og:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/header-navidad.jpg" />
 
+
 <?php } else if( in_array($node->nid, $navidad_list) &&  in_array($node->nid,$donativos_form_list) ) { ?>
 
     <meta property="og:url" content="https://crm.es.amnesty.org/haz-un-donativo-por-navidad" />
@@ -48,21 +49,20 @@
 
 <?php } ?>
 
-<!--Twitter Cards-->
-<meta name="twitter:url" content="http://testing.estresarte.com/amnistia/navidad/">
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@amnistiaespana">
-<meta name="twitter:creator" content="@amnistiaespana">
-<meta name="twitter:title" content="#JusticiaPorNavidad – Amnistía Internacional">
-<meta name="twitter:description" content="Miles de personas 'desaparecidas' durante la guerra civil y el franquismo no volvieron a casa. Firma para pedir al gobierno que este año no se olvide de ellas.">
-<meta name="twitter:image" content="http://testing.estresarte.com/amnistia/navidad/images/compartir-tw.png">
-<meta name="twitter:image:width" content="1024">
-<meta name="twitter:image:height" content="512">
-<meta name="twitter:image:alt" content="#JusticiaPorNavidad – Amnistía Internacional">
-
 <!-- Botones de redes sociales -->
 
 <?php if( in_array($node->nid, $navidad_list) ) { ?>
+    <!--Twitter Cards-->
+    <meta name="twitter:url" content="https://www.es.amnesty.org/justiciapornavidad/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@amnistiaespana">
+    <meta name="twitter:creator" content="@amnistiaespana">
+    <meta name="twitter:title" content="#JusticiaPorNavidad – Amnistía Internacional">
+    <meta name="twitter:description" content="Miles de personas 'desaparecidas' durante la guerra civil y el franquismo no volvieron a casa. Firma para pedir al gobierno que este año no se olvide de ellas.">
+    <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/header-navidad.jpg">
+    <meta name="twitter:image:width" content="1024">
+    <meta name="twitter:image:height" content="512">
+    <meta name="twitter:image:alt" content="#JusticiaPorNavidad – Amnistía Internacional">
     <script>
       var urlActual = 'https://www.es.amnesty.org/justiciapornavidad';
       var tituloActual = 'Miles de familias tienen un deseo que lleva años sin cumplirse: firma y ayúdales a conseguirlo:';
@@ -73,6 +73,17 @@
       var compartirViaTW = 'amnistiaespana';
     </script>
 <?php } else if( in_array($node->nid,$socixs_gracias_list) && $cat ){ ?>
+    <!--Twitter Cards-->
+    <meta name="twitter:url" content="https://crm.es.amnesty.org/unete-a-amnistia/cat/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@AmnistiaCAT">
+    <meta name="twitter:creator" content="@AmnistiaCAT">
+    <meta name="twitter:title" content="Uneix-te a Amnistia Internacional">
+    <meta name="twitter:description" content="Fes-te soci/a d\'Amnistia Internacional i recolza la nostra feina.">
+    <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
+    <meta name="twitter:image:width" content="1024">
+    <meta name="twitter:image:height" content="512">
+    <meta name="twitter:image:alt" content="Uneix-te a Amnistia Internacional">
     <script>
       var urlActual = 'https://crm.es.amnesty.org/unete-a-amnistia/cat/';
       var tituloActual = 'Fes-te soci/a d\'Amnistia Internacional i recolza la nostra feina.';
@@ -83,6 +94,17 @@
       var compartirViaTW = 'AmnistiaCAT';
     </script>
 <?php } else if( in_array($node->nid,$socixs_gracias_list) && !$cat ) { ?>
+    <!--Twitter Cards-->
+    <meta name="twitter:url" content="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@amnistiaespana">
+    <meta name="twitter:creator" content="@amnistiaespana">
+    <meta name="twitter:title" content="Únete a Amnistía Internacional">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
+    <meta name="twitter:image:width" content="1024">
+    <meta name="twitter:image:height" content="512">
+    <meta name="twitter:image:alt" content="Únete a Amnistía Internacional">
     <script>
       var urlActual = 'https://crm.es.amnesty.org/unete-a-amnistia/';
       var tituloActual = 'Hazte socio/a de Amnistía Internacional y apoya nuestro trabajo.';
@@ -93,6 +115,17 @@
       var compartirViaTW = 'amnistiaespana';
     </script>
 <?php } else if( in_array($node->nid,$donativos_gracias_list) && $cat ) { ?>
+  <!--Twitter Cards-->
+  <meta name="twitter:url" content="https://crm.es.amnesty.org/haz-un-donativo/cat/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@AmnistiaCAT">
+  <meta name="twitter:creator" content="@AmnistiaCAT">
+  <meta name="twitter:title" content="Fes un donatiu a Amnistia Internacional">
+  <meta name="twitter:description" content="Fes un donatiu a Amnistia Internacional i recolza la nostra feina.">
+  <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
+  <meta name="twitter:image:width" content="1024">
+  <meta name="twitter:image:height" content="512">
+  <meta name="twitter:image:alt" content="Fes un donatiu a Amnistia Internacional">
   <script>
     var urlActual = 'https://crm.es.amnesty.org/unete-a-amnistia/cat/';
     var tituloActual = 'Fes un donatiu a Amnistia Internacional i recolza la nostra feina.';
@@ -103,6 +136,17 @@
     var compartirViaTW = 'AmnistiaCAT';
   </script>
 <?php } else if( in_array($node->nid,$donativos_gracias_list) && !$cat ) { ?>
+  <!--Twitter Cards-->
+  <meta name="twitter:url" content="https://crm.es.amnesty.org/haz-un-donativo/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@amnistiaespana">
+  <meta name="twitter:creator" content="@amnistiaespana">
+  <meta name="twitter:title" content="Haz un donativo a Amnistía Internacional">
+  <meta name="twitter:description" content="Haz un donativo a Amnistía Internacional y apoya nuestro trabajo.">
+  <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
+  <meta name="twitter:image:width" content="1024">
+  <meta name="twitter:image:height" content="512">
+  <meta name="twitter:image:alt" content="Haz un donativo a Amnistía Internacional">
   <script>
     var urlActual = 'https://crm.es.amnesty.org/unete-a-amnistia/';
     var tituloActual = 'Haz un donativo a Amnistía Internacional y apoya nuestro trabajo.';
