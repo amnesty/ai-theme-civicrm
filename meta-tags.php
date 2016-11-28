489bf328554eae574f3b10e36adb056b4dfa94a7
Helena <hgarcia@es.amnesty.org>
2016-11-28 11:35:30 +0100

    arregladas twitter cards

--- meta-tags.php
+++ meta-tags.php
@@ -41,16 +41,16 @@
 
     <meta property="og:url" content="https://crm.es.amnesty.org/unete-a-amnistia/cat" />
     <meta property="og:type" content="website" />
-    <meta name="title" property="og:title" content="Hazte socio/a de Amnistía Internacional y apoya nuestro trabajo" />
-    <meta name="description" property="og:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos." />
+    <meta name="title" property="og:title" content="Fes-te soci/a d'Amnistia Internacional i recolza la nostra feina" />
+    <meta name="description" property="og:description" content="Amnistia Internacional és una organizació democràtica i independent que no accepta fons que puguin comprometre la seva feina. Es finança exclusivament gràcies a les aportacions de persones compromeses amb la idea d'aconseguir un món més just. La teva col·laboració, per petita que sigui, és l'única cosa que ens permet seguir defensant els drets humans, així com investigar i denunciar abusos que es duen a terme a tot el món. Fes-te soci/a i recolza la nostra feina. Quants més siguem, més força tindrem." />
     <meta property="og:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>" />
     <!--Twitter Cards-->
-    <meta name="twitter:url" content="https://crm.es.amnesty.org/unete-a-amnistia/cat/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
+    <meta name="twitter:url" content="https://crm.es.amnesty.org/unete-a-amnistia/cat/?utm_campaign=comp&utm_term=Amnesty&utm_content=form_socios_twitter&utm_medium=social_com&utm_source=twitter">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@AmnistiaCAT">
     <meta name="twitter:creator" content="@AmnistiaCAT">
-    <meta name="twitter:title" content="Uneix-te a Amnistia Internacional">
-    <meta name="twitter:text:description" content="Fes-te soci/a d\'Amnistia Internacional i recolza la nostra feina.">
+    <meta name="twitter:title" content="Uneix-te a Amnistia Internacional i recolza la nostra feina.">
+    <meta name="twitter:text:description" content="Amnistia Internacional és una organizació democràtica i independent que no accepta fons que puguin comprometre la seva feina. Es finança exclusivament gràcies a les aportacions de persones compromeses amb la idea d'aconseguir un món més just. La teva col·laboració, per petita que sigui, és l'única cosa que ens permet seguir defensant els drets humans, així com investigar i denunciar abusos que es duen a terme a tot el món. Fes-te soci/a i recolza la nostra feina. Quants més siguem, més força tindrem.">
     <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
     <meta name="twitter:image:width" content="1024">
     <meta name="twitter:image:height" content="512">
@@ -58,18 +58,18 @@
 
 <?php } else if( in_array($node->nid,$socixs_form_list) && !$cat ) { ?>
 
-    <meta property="og:url" content="https://crm.es.amnesty.org/unete-a-amnistia/" />
+    <meta property="og:url" content="https://crm.es.amnesty.org/unete-a-amnistia/?utm_campaign=comp&utm_term=Amnesty&utm_content=form_socios_twitter&utm_medium=social_com&utm_source=twitter" />
     <meta property="og:type" content="website" />
-    <meta name="title" property="og:title" content="Fes-te soci/a d'Amnistia Internacional i recolza la nostra feina" />
-    <meta name="description" property="og:description" content="Amnistia Internacional és una organizació democràtica i independent que no accepta fons que puguin comprometre la seva feina. Es finança exclusivament gràcies a les aportacions de persones compromeses amb la idea d'aconseguir un món més just. La teva col·laboració, per petita que sigui, és l'única cosa que ens permet seguir defensant els drets humans, així com investigar i denunciar abusos que es duen a terme a tot el món. Fes-te soci/a i recolza la nostra feina. Quants més siguem, més força tindrem." />
+    <meta name="title" property="og:title" content="Hazte socio/a de Amnistía Internacional y apoya nuestro trabajo" />
+    <meta name="description" property="og:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos." />
     <meta property="og:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>" />
-    <!--Twitter Cards-->
+      <!--Twitter Cards-->
     <meta name="twitter:url" content="https://crm.es.amnesty.org/unete-a-amnistia/?utm_source=twitter&utm_campaign=comp&utm_medium=social_com&utm_term=Killings_disappearances&utm_content=Web_petition-justiciapornavidad-twitter">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@amnistiaespana">
     <meta name="twitter:creator" content="@amnistiaespana">
-    <meta name="twitter:title" content="Únete a Amnistía Internacional">
-    <meta name="twitter:text:description" content="Hazte socio de Amnistía Internacional y apoya nuestro trabajo.">
+    <meta name="twitter:title" content="Hazte socio/a de Amnistía Internacional y apoya nuestro trabajo.">
+    <meta name="twitter:text:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos.">
     <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
     <meta name="twitter:image:width" content="1024">
     <meta name="twitter:image:height" content="512">
@@ -77,7 +77,7 @@
 
 <?php } else if( in_array($node->nid,$donativos_form_list) && $cat ) { ?>
 
-    <meta property="og:url" content="https://crm.es.amnesty.org/haz-un-donativo/cat" />
+    <meta property="og:url" content="https://crm.es.amnesty.org/haz-un-donativo/cat/?utm_campaign=comp&utm_term=Amnesty&utm_content=form_donativo_twitter&utm_medium=social_com&utm_source=twitter" />
     <meta property="og:type" content="website" />
     <meta name="title" property="og:title" content="Fes un donatiu a Amnistia Internacional i recolza la nostra feina" />
     <meta name="description" property="og:description" content="Amnistia Internacional és una organizació democràtica i independent que no accepta fons que puguin comprometre la seva feina. Es finança exclusivament gràcies a les aportacions de persones compromeses amb la idea d'aconseguir un món més just. La teva col·laboració, per petita que sigui, és l'única cosa que ens permet seguir defensant els drets humans, així com investigar i denunciar abusos que es duen a terme a tot el món. Fes-te soci/a i recolza la nostra feina. Quants més siguem, més força tindrem." />
@@ -87,8 +87,8 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@AmnistiaCAT">
     <meta name="twitter:creator" content="@AmnistiaCAT">
-    <meta name="twitter:title" content="Fes un donatiu a Amnistia Internacional">
-    <meta name="twitter:text:description" content="Fes un donatiu a Amnistia Internacional i recolza la nostra feina.">
+    <meta name="twitter:title" content="Fes un donatiu a Amnistia Internacional i recolza la nostra feina.">
+    <meta name="twitter:text:description" content="Amnistia Internacional és una organizació democràtica i independent que no accepta fons que puguin comprometre la seva feina. Es finança exclusivament gràcies a les aportacions de persones compromeses amb la idea d'aconseguir un món més just. La teva col·laboració, per petita que sigui, és l'única cosa que ens permet seguir defensant els drets humans, així com investigar i denunciar abusos que es duen a terme a tot el món. Fes-te soci/a i recolza la nostra feina. Quants més siguem, més força tindrem.">
     <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
     <meta name="twitter:image:width" content="1024">
     <meta name="twitter:image:height" content="512">
@@ -96,7 +96,7 @@
 
 <?php } else if( in_array($node->nid,$donativos_form_list) && !$cat ) { ?>
 
-    <meta property="og:url" content="https://crm.es.amnesty.org/haz-un-donativo" />
+    <meta property="og:url" content="https://crm.es.amnesty.org/haz-un-donativo/?utm_campaign=comp&utm_term=Amnesty&utm_content=form_donativo_twitter&utm_medium=social_com&utm_source=twitter" />
     <meta property="og:type" content="website" />
     <meta name="title" property="og:title" content="Haz un donativo a Amnistía Internacional y apoya nuestro trabajo" />
     <meta name="description" property="og:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos." />
@@ -106,8 +106,8 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@amnistiaespana">
     <meta name="twitter:creator" content="@amnistiaespana">
-    <meta name="twitter:title" content="Haz un donativo a Amnistía Internacional">
-    <meta name="twitter:text:description" content="Haz un donativo a Amnistía Internacional y apoya nuestro trabajo.">
+    <meta name="twitter:title" content="Haz un donativo a Amnistía Internacional y apoya nuestro trabajo.">
+    <meta name="twitter:text:description" content="Amnistía Internacional es una organización democrática e independiente que no acepta fondos que puedan comprometer su trabajo. Se financia exclusivamente gracias a las aportaciones de personas comprometidas con la idea de conseguir un mundo más justo. Tu colaboración, por pequeña que sea, es lo único que nos permite seguir defendiendo los derechos humanos, así como investigando y denunciando abusos que se cometen en todo el mundo. Hazte socio/a y apoya nuestro trabajo. Cuantos más seamos, más fuerza tendremos.">
     <meta name="twitter:image" content="https://crm.es.amnesty.org/sites/all/themes/ai-theme-civicrm/ai-formulario/images/<?php print $img_header; ?>">
     <meta name="twitter:image:width" content="1024">
     <meta name="twitter:image:height" content="512">
