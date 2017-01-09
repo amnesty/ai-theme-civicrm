427ab50d7f3c83acd45f123f459b11c2b42ab735
Helena <hgarcia@es.amnesty.org>
2017-01-09 12:40:07 +0100

    [formularios telemkg] conservar parámetros GET en caso de error

--- ai-formulario/js/socixs-form.js
+++ ai-formulario/js/socixs-form.js
@@ -70,7 +70,7 @@ jQuery(function($) {
     var form = $('.webform-client-form');
     if( !form.hasClass('preview') ){
         form.submit( function() {
-          form.attr("action",  window.location.pathname + '/?preview=1&' + get_params);
+          form.attr("action",  window.location.pathname + '?preview=1&' + get_params);
         });
     }
     else {
