d96c31f4d6e16b89f6b75a23d5f56eb8461313f1
Helena <hgarcia@es.amnesty.org>
2017-01-19 12:41:14 +0100

    añadido popup de validacion de cuota minima, solo para formularios publicos, no de telemarketing etc

--- styles_scripts.php
+++ styles_scripts.php
@@ -27,8 +27,12 @@
       <link rel="stylesheet" type="text/css" href="<?php print $form_path; ?>/css/donativos-form.css">
   <?php } ?>
 
+  <?php if ( in_array($node->nid, $socixs_form_list) && !in_array($node->nid, $telemkg_form_list) ) { ?>
+      <script type="text/javascript" src="<?php print $form_path; ?>/js/membership-validator.js"></script>
+  <?php } ?>
+
 <?php } ?>
-<script type="text/javascript" src="<?php print $form_path; ?>/js/membership-validator.js"></script>
+
 <?php if( in_array($node->nid, $telemkg_form_list) ){ ?>
       <script type="text/javascript" src="<?php print $form_path; ?>/js/autofill.js"></script>
 <?php } ?>
