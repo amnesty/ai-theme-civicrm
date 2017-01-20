2e4b93b73e01da07717272189457f27c01c55239
Helena <hgarcia@es.amnesty.org>
2017-01-20 12:40:48 +0100

    externalizados textos a strings.php + cambiada foto cabecera navidad

---
+++ strings.php
@@ -0,0 +1,58 @@
+<?php
+
+  /* Textos intro formulario */
+
+  $texto_intro_donativo_navidad = '<span>Has llegado hasta aquí porque algo dentro de ti te dice que hay millones de razones para colaborar. Colabora con el trabajo que hacemos por el derecho a la verdad, justicia y reparación de todas las personas.</span>
+    <span>Haz un donativo y ayúdanos a seguir trabajando para mantener viva la lucha de miles de personas que siguen pidiendo justicia.</span>';
+  //$texto_intro_donativo_navidad = '<span>Has llegado hasta aquí porque algo dentro de ti te dice que hay millones de razones para colaborar. <b>Decora tu casa y a la vez colabora</b> con el trabajo que hacemos por el derecho a la verdad, justicia y reparación de todas las personas.</span><br/>
+    //<span><b>Haz un donativo y recibirás en tu casa esta bola</b> para que la cuelgues en el árbol, en tu puerta o en el lugar de tu casa que prefieras. Así ayudarás a mantener vivos los deseos que llevan años sin cumplirse.</span>';
+
+  $texto_intro_antevenio_BE = '<span>Miles de personas huyen del infierno de las bombas y buscan un hogar seguro. Personas ancianas, mujeres embarazadas y bebés; niños y niñas separados de sus familias. En Amnistía Internacional levantamos nuestra voz en defensa de todas las personas refugiadas para garantizarles un lugar seguro donde vivir, y devolver a cada niño y niña la infancia que se merece.</span><br>
+    <span>Renunciamos a cualquier subvención porque nuestra independencia está por encima de todo y, gracias a personas como tú, podemos denunciar sin presiones cualquier violación de los derechos humanos.</span><br>
+    <span><b>Gracias por creer en un mundo más justo.</b></span>';
+
+  $texto_intro_antevenio_CD = '<span>En Amnistía Internacional luchamos para conseguir igualdad y protección para todas las mujeres y niñas.</span><br>
+    <span>Renunciamos a cualquier subvención porque nuestra independencia está por encima de todo y, gracias a personas como tú, podemos denunciar sin presiones violación de los derechos humanos.</span><br>
+    <span><b>Gracias por creer en un mundo más justo.</b></span>';
+
+  $texto_intro_donativo = '<span> Has llegado hasta aquí porque algo dentro de ti te dice que hay millones de razones para colaborar. La fundamental es que quieres cambiar el mundo, y no sabes cuanto nos alegra, porque sabemos que podemos contar contigo para lograrlo. <b>Gracias por creer en un mundo más justo.<b/></span>';
+
+  $texto_intro_donativo_cat = '<span> Has arribat fins aquí perquè alguna cosa dins teu diu que hi ha milions de raons per a col·laborar. La fonamental és que vols canviar el món, i no saps com ens n\'alegrem, perquè sabem que podem comptar amb tu per aconseguir-ho. <b>Gràcies per creure en un món més just.<b/></span>';
+
+  $texto_intro_socixs = '<span>Tu ayuda hace posible que podamos renunciar a subvenciones de gobiernos y partidos políticos, porque nuestra independencia está por encima de todo. </span>
+    <span>Es gracias a personas como tú que nos apoyáis económicamente por lo que podemos denunciar sin presiones cualquier violación de los derechos humanos. </span>
+    <span><b>Gracias por creer en un mundo más justo.</b></span>';
+
+  $texto_intro_socixs_cat = '<span>La teva ajuda fa possible que puguem renunciar a subvencions de governs i partits polítics, perquè la nostra independència està per sobre de tot. </span>
+    <span>És gràcies a persones com tu que ens doneu suport econòmic que podem denunciar sense pressions de cap mena qualsevol violació dels drets humans. </span>
+    <span><b>Gràcies per creure en un món més just.</b></span>';
+
+  /* Botones */
+
+  $boton_unete = 'Únete a Amnistía Internacional';
+  $boton_unete_cat = 'Uneix-te a Amnistia Internacional';
+
+  /* Cajas laterales */
+
+  $titulo_caja_buenas_noticias = '<h3>¡Buenas Noticias!</h3>';
+  $texto_caja_buenas_noticias = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <a class="link-caja-buenas-noticias" target="_blank" href="https://www.es.amnesty.org/en-que-estamos/para-celebrar" title="buenas noticias: en qué estamos - para celebrar">CONOCE MÁS</a>.
+    </p>';
+
+  $titulo_caja_ventajas = "<h3>¿Conoces las ventajas fiscales de ser socio/a?</h3>";
+  $titulo_caja_ventajas_donativo = "<h3>¿Conoces las ventajas fiscales de hacer una donación?</h3>";
+  $titulo_caja_ventajas_cat = '<h3>Coneixes els avantatges fiscals de ser soci/a?</h3>';
+  $titulo_caja_ventajas_donativo_cat = '<h3>Coneixes els avantatges fiscals de fer un donatiu?</h3>';
+  $texto_caja_ventajas = "<p>Todas tus aportaciones desgravan un 75% los primeros 150€. A partir de esa cifra, el 30%. Si en los últimos tres años se han mantenido o aumentado las aportaciones a la organización, la desgravación sube a un 35% para premiar tu fidelidad. Estas deducciones no aplican en País Vasco y Navarra.";
+  $texto_caja_ventajas_cat = "<p>Totes les teves aportacions desgraven un 75% els primers 150€. A partir d'aquesta xifra, el 30%. Si els últims 3 anys has mantingut o augmentat l'aportació a l'organització, la desgravació puja fins un 35% per a premiar la teva fidelitat. Aquestes deduccions no s'apliquen al País Basc ni a Navarra.";
+
+  $titulo_caja_formas_pago_cat = "<h3>Altres mètodes de pagament</h3>";
+  $texto_caja_formas_pago_cat = "<p>Si tens algún problema en realitzar la teva donació o vols informació sobre altres mètodes de pagament o sobre quotes reduïdes, truca'ns al <b>913101277</b> (extensions <b>30</b> i <b>37</b>) o escriu-nos a <a href=\"mailto:socios@es.amnesty.org\" title=\"socios@es.amnesty.org\">socios@es.amnesty.org</a></p>";
+  $titulo_caja_formas_pago = "<h3>Otras formas de pago</h3>";
+  $texto_caja_formas_pago = "<p>Si tienes algún problema al realizar tu donación o quieres información sobre otras formas de pago o sobre cuotas reducidas, llámanos a <b>913101277</b> (extensiones <b>30</b> y <b>37</b>) o escribe a <a href=\"mailto:socios@es.amnesty.org\" title=\"socios@es.amnesty.org\">socios@es.amnesty.org</a></p>";
+
+  $titulo_caja_transparencia_cat = "<h3>Transparència, el nostre compromís</h3>";
+  $titulo_caja_transparencia = "<h3>Transparencia, nuestro compromiso</h3>";
+  $texto_caja_transparencia = "<p>El 100% de nuestros recursos los destinamos a luchar por los derechos humanos en todo el mundo. Nuestras cuentas son públicas y puedes verlas en nuestra web.</p>";
+  $texto_caja_transparencia_cat = "<p>El 100% dels nostres recursos els destinem a lluitar pels drets humans arreu del món. Els nostres comptes són públics i pots veure'ls a la nostra web.</p>";
+?>
