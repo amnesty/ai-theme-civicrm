2625257125f53cb9a2cf0a9e216b02bb937ff98b
Helena <hgarcia@es.amnesty.org>
2017-04-19 17:25:09 +0200

    Piwik: añadidos pixels para google adwords

--- piwik-prod.php
+++ piwik-prod.php
@@ -50,7 +50,7 @@ if( in_array($node->nid, $donativos_gracias_list) && !isset($_GET['m']) ){ ?>
 <?php
 }
 
-// Excluimos a Attel del tracking
+// Excluimos a formualrios de telemarketing del tracking
 if ( !in_array($node->nid, $telemkg_form_list) &&
    ( in_array($node->nid, $socixs_form_list) || in_array($node->nid, $socixs_gracias_list)
    || in_array($node->nid, $donativos_form_list) || in_array($node->nid, $donativos_gracias_list) )
@@ -94,9 +94,57 @@ if ( !in_array($node->nid, $telemkg_form_list) &&
   <!-- End Piwik Code -->
 <?php }
 
+// Píxeles Adwords
+if(isset($_COOKIE['cookieAlert3']) && $_COOKIE['cookieAlert3'] == 1){ // si ha aceptado las cookies
+
+  if( isset($_GET["get_campaign"]) && $_GET["get_campaign"] == 'anunggl_display' || $_SESSION['pk_campaign'] == 'anunggl_display' || $_SESSION['utm_campaign'] == 'anunggl_display') { // display
+    <!-- Google Code for pixel_socios Conversion Page -->
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 966452768;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "2_6jCLezgGwQoMzrzAM";
+    var google_conversion_value = 9.00;
+    var google_conversion_currency = "EUR";
+    var google_remarketing_only = false;
+    /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+    <div style="display:inline;">
+    <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/966452768/?value=9.00&amp;currency_code=EUR&amp;label=2_6jCLezgGwQoMzrzAM&amp;guid=ON&amp;script=0"/>
+    </div>
+    </noscript>
+  }
+  else if (isset($_GET["get_campaign"]) && $_GET["get_campaign"] == 'anunggl' || $_SESSION['pk_campaign'] == 'anunggl' || $_SESSION['utm_campaign'] == 'anunggl'){ // grants
+    <!-- Google Code for pixel_socios_grant Conversion Page -->
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 973137582;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "xArOCN2-xXAQrs2D0AM";
+    var google_remarketing_only = false;
+    /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+    <div style="display:inline;">
+    <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/973137582/?label=xArOCN2-xXAQrs2D0AM&amp;guid=ON&amp;script=0"/>
+    </div>
+    </noscript>
+  }
+  
+}
+
 /***************** Mapas de calor ***************/
 
-if( ( $node->nid == $socixs_form || $node->nid == $socixs_form_A ) && !in_array($node->nid, $telemkg_form_list) ) { ?>
+/*if( ( $node->nid == $socixs_form || $node->nid == $socixs_form_A ) && !in_array($node->nid, $telemkg_form_list) ) { ?>
 	<script type="text/javascript" src="//estadisticas.es.amnesty.org/piwik/plugins/ClickHeat/libs/js/clickheat.js"></script>
 	<script type="text/javascript">
 		clickHeatSite = 1;
@@ -120,4 +168,4 @@ if( ( $node->nid == $socixs_form || $node->nid == $socixs_form_A ) && !in_array(
                 clickHeatServer = 'http://estadisticas.es.amnesty.org/piwik/plugins/ClickHeat/libs/click.php';
                 initClickHeat();
         </script>
-<?php } ?>
+<?php } ?>*/
