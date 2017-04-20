31169c17ebf7b2fec72028e06e05edec1f1312a8
Helena <hgarcia@es.amnesty.org>
2017-04-20 13:55:50 +0200

    Añadido banner para politica de cookies

---
+++ cookies.php
@@ -0,0 +1,5 @@
+<div id="cookie-law-info-bar">
+  <a href="#" id="close_cookie"  class="small cli-plugin-button cli-plugin-main-button" >X</a>
+  </span>
+  <span>Al navegar por este sitio aceptas nuestra <a href="https://www.es.amnesty.org/politica-de-privacidad/" target="_blank" class="cli-plugin-main-link" >Política de Privacidad.</a> Esta web utiliza <a href="https://www.es.amnesty.org/cookies/" target="_blank"  class="cli-plugin-main-link">cookies.</a> <a href="#" id="cookie_action_close_header"  class="small cli-plugin-button cli-plugin-main-button" >Acepto</a><a href="#" id="cookie_action_no"  class="small" style="color:#ffffff; font-size: 10px;" >No acepto</a></span>
+</div>
