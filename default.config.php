7cd707ae146edd80c6a198117dde054ddf3747ef
Helena <hgarcia@es.amnesty.org>
2017-05-19 18:21:42 +0200

    añadido config default

---
+++ default.config.php
@@ -0,0 +1,59 @@
+<?php
+
+// Auto e-mail saver
+$email_saver=0;
+
+$is_form_mobile=0;
+
+// Excluidos de cabecera, lateral, footer...
+$excluded_header_list = array(99);
+
+// Id's de las páginas/nodos de Drupal
+$socixs_form_list = array(99);
+$socixs_gracias_list = array(99);
+$donativos_form_list = array(99);
+$donativos_gracias_list = array(99);
+$navidad_list = array(99);
+$nosevende_list = array(99);
+
+$socixs_form = 99; // id de la página que contiene el forumulario de socixs en este contexto
+$socixs_gracias = 99; // id de la página de gracias del formulario de socixs
+$socixs_form_cat = 99;
+$socixs_gracies = 99;
+
+$donativos_form = 99;
+$donativos_gracias = 99;
+$donativos_form_cat = 99;
+$donativos_gracias_cat = 99;
+
+$area_privada = 99;
+$firma_navidad = 99;
+
+// Índices
+$donativo_idx=99;
+$donativo_idx2=99;
+$metodo_pago_idx=99;
+
+// Test A/B
+$socixs_form_A = 99;
+$socixs_form_B = 99;
+$socixs_gracias_A = 99;
+$socixs_gracias_B = 99;
+
+// Telemarketing
+$telemkg_form_list = array(99);
+
+// Antevenio
+$antevenio_list = array(99);
+$antevenio_form_A=99;
+$antevenio_form_B=99;
+$antevenio_form_C=99;
+$antevenio_form_D=99;
+$antevenio_form_E=99;
+$antevenio_gracias_A=99;
+$antevenio_gracias_B=99;
+$antevenio_gracias_C=99;
+$antevenio_gracias_D=99;
+$antevenio_gracias_E=99;
+
+?>
