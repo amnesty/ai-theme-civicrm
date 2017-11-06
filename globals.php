af8b9f9f14c8b3a2d602370038080f0eedaab5b0
frodriguez <frodriguez@es.amnesty.org>
2017-11-06 15:29:01 +0100

    Cabecera formulario de Donacion

--- globals.php
+++ globals.php
@@ -30,8 +30,11 @@ if ($node->nid == $antevenio_form_B || $node->nid == $antevenio_gracias_B || $no
 } else if( in_array($node->nid,$navidad_list) ){ // campaña navidad
         $img_header = "header-navidad.jpg";
         $extra_class="responsive--bg-nav";
-} else if( in_array($node->nid,$loteria_navidad_list) ){ // campaña loteria de navidad 2017
+} else if(in_array($node->nid,$loteria_navidad_list) ){ // campaña loteria de navidad 2017
         $img_header = "header-loteria.jpg";
+        if(in_array($node->nid,$donativos_form_list)){
+          $img_header = "header-estrella.png";
+        }
         $extra_class="responsive--bg-nav";
 } else if( in_array($node->nid,$nosevende_list) && ( in_array($node->nid,$donativos_form_list) || in_array($node->nid,$donativos_gracias_list) ) ) { // donativos campaña nosevende
         $img_header = "header-nosevende.jpg";
