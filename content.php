d833b130160f6130fd6855f4a54a76bfd0c62abf
frodriguez <frodriguez@es.amnesty.org>
2017-11-06 11:04:45 +0100

    Cambios

--- content.php
+++ content.php
@@ -39,7 +39,7 @@
                 <?php if( in_array($node->nid, $navidad_list) && in_array($node->nid, $donativos_form_list) ) { ?>
                     <!--img src="<?php print $images_path; ?>bola-gracias.png" class="bola-navidad" /-->
                 <?php } elseif( in_array($node->nid, $loteria_navidad_list) && in_array($node->nid, $donativos_form_list) ) { ?>
-                        <!--<img src="<?php print $images_path; ?>estrella-regalo.png" class="estrella-navidad" />-->
+                        <img src="<?php print $images_path; ?>estrella-regalo.png" class="estrella-navidad" />
                 <?php } ?>
               <?php } ?>
   		        <div class="box-form-es" <?php if( $excluded_header_list != null || in_array($node->nid, $excluded_header_list) ){ ?>style="width: 100%;" <?php } ?> >
