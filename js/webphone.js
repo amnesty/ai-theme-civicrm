f5b99d6f0371a030bdd4555ea072a193c2f37313
Helena <hgarcia@es.amnesty.org>
2018-07-11 16:13:33 +0200

    añadido extra script propio para sobreescribir funcion que falla

--- js/webphone.js
+++ js/webphone.js
@@ -1,12 +1,11 @@
-/**
- * Append Webphone button by object
- */
-function appendWphButtonByObject() {
-    jQuery(function($) {
-      $('object[classid="webphone"]').each(function(i){
-       if (typeof $(this).attr('id') != 'undefined' && $(this).attr('id') != '') {
-         appendButtonWphByObj($(this).attr('id'));
-       }
-    });
-  });
-}
+var fn_old = appendWphButtonByObject; // `old` won't be overwritten
+
+appendWphButtonByObject = function(){
+            jQuery(function($) {
+              $('object[classid="webphone"]').each(function(i){
+               if (typeof $(this).attr('id') != 'undefined' && $(this).attr('id') != '') {
+                 appendButtonWphByObj($(this).attr('id'));
+               }
+            });
+          });
+        };
