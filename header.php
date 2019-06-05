6cb9214ad1069dc983358a391fb375c7fa44eb1d
Helena <hgarcia@es.amnesty.org>
2019-06-05 17:24:28 +0200

    Añadido clicktocall para formularios de campaña acoso

--- header.php
+++ header.php
@@ -38,6 +38,8 @@
           <?php } else { ?>
             <object id="3005" type="button/webphone" classid="webphone" style="display: none;"></object>
           <?php } ?>
+        <?php } else if( (in_array($node->nid, $socixs_acoso_list) { /* formularios acoso */ ?>
+            <object id="3211" type="button/webphone" classid="webphone" style="display: none;"></object>
         <?php } else if( (in_array($node->nid, $socixs_form_list) || /* formulario socixs */
                 in_array($node->nid, $socixs_form_list_mobile)) && /* formularios moviles de asociacion */
               !in_array($node->nid, $telemkg_form_list) ) /* no en formularios tmk */ { ?>
