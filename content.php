a388c85ecc1cf35d55a75b56a7a8cc7ecdff18b4
Frodriguezgordo <frodriguez@es.amnesty.org>
2021-09-01 11:03:52 +0200

    Texto

--- content.php
+++ content.php
@@ -23,7 +23,7 @@
                         echo $texto_intro_antevenio_CD;
                   } elseif($node->nid  == $donativos_islamofobia) {
                         echo $texto_intro_donativo_islamofobia;
-                      } elseif($node->nid  == $donativo_afganistan) {
+                  } elseif($node->nid  == $donativo_afganistan) {
                             echo $texto_intro_donativo_afganistan;
                   } elseif( in_array($node->nid, $donativos_form_list) ) {
                         if( $cat == 0 ) {
