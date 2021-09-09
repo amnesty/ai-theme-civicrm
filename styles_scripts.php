483bc4594127f6920911b06a20ca93afc501f58f
Frodriguezgordo <frodriguez@es.amnesty.org>
2021-09-09 15:51:24 +0200

    Se incluye script para la carga de las utms en los formularios de donativos móviles

--- styles_scripts.php
+++ styles_scripts.php
@@ -52,7 +52,8 @@
     <script type="text/javascript" src="<?php print $form_path; ?>/js/membership-validator.js"></script>
 <?php } ?>
 <!-- Si es dipositivo móvil -->
-<?php if( !empty($socixs_form_list_mobile) && in_array($node->nid, $socixs_form_list_mobile) && $mobile){ ?>
+<?php if( (!empty($socixs_form_list_mobile) && in_array($node->nid, $socixs_form_list_mobile))
+        || (in_array($node->nid, $donativos_form_list_mobile) || in_array($node->nid, $donativos_gracias_list_mobile)) && $mobile){ ?>
   <script type="text/javascript" src="<?php print $form_path; ?>/js/socixs-form-m.js"></script>
   <!--<link rel="stylesheet" type="text/css" href="<?php print $form_path; ?>/css/donativos-form.css">-->
 <?php } ?>
