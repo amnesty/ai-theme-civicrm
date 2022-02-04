ba7769c708872d8399da3c062246057a56f9242f
Frodriguezgordo <frodriguez@es.amnesty.org>
2022-02-04 13:08:22 +0100

    Fix bug

--- template.php
+++ template.php
@@ -10,6 +10,9 @@
 /**
  * Bootstrap theme wrapper function for the primary menu links.
  */
-function encoremed_bootstrap_menu_tree__primary(&$variables) {
-  return '<ul class="menu nav navbar-nav navbar-right">' . $variables['tree'] . '</ul>';
+ if (!function_exists("encoremed_bootstrap_menu_tree__primary")) {
+   /** Bootstrap theme wrapper function for the primary menu links. **/
+   function encoremed_bootstrap_menu_tree__primary(&$variables) {
+     return '<ul class="menu nav navbar-nav navbar-right">' . $variables['tree'] . '</ul>';
+   }
 }
