b9f21079600ca9f9bff94321cf7bc10047dcf3e8
Ricardo <rbellomo@es.amnesty.org>
2024-10-16 10:48:51 +0200

    fix bug

--- styles_scripts.php
+++ styles_scripts.php
@@ -38,7 +38,7 @@
 <?php if( ($node->nid == $socixs_form || $node->nid == $socixs_form_cat || in_array($node->nid, $loteria_navidad_list) || in_array($node->nid, $socixs_form_list)) && !$mobile){ ?>
   <script defer type="text/javascript" src="<?php print $form_path; ?>/js/cuotas.js"></script>
   <link rel="stylesheet" type="text/css" href="<?php print $form_path; ?>/css/cuotas.css">
-<?php } else if ( $node->nid == $socixs_form_m || in_array($node>nid, $socixs_form_list_mobile)) { ?>
+<?php } else if ( $node->nid == $socixs_form_m || in_array($node->nid, $socixs_form_list_mobile)) { ?>
   <script defer type="text/javascript" src="<?php print $form_path; ?>/js/cuotas-m.js"></script>
 <?php } ?>
 
